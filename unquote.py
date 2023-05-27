2de76f7d31f8cadc927a80d359ee669fe71bf792
lbibic <luka.krnjevo@gmail.com>
2023-05-27 19:24:44 +0200

    init local commit to sync branches

---
+++ unquote.py
@@ -0,0 +1,28 @@
+import os
+from vott_reader import get_vott_info
+
+
+def match_vott_to_yolo(vott_str):
+
+    url = vott_str.replace("%", '_')
+    url = url.replace(".png", ".txt")
+
+    return url
+
+
+if __name__ == '__main__':
+
+    asset_txt_map = dict()
+
+    info_map = get_vott_info()
+    for asset_id in info_map:
+        txt_file = match_vott_to_yolo(info_map[asset_id])
+        yolo_path = os.path.join('yolo_detection_txt_files', txt_file)
+
+        if os.path.isfile(yolo_path):
+            print('file exists in folder')
+            asset_txt_map[asset_id] = txt_file
+        else:
+            print('file does not exist in folder!')
+
+    print(asset_txt_map)
---
+++ vott_reader.py
@@ -0,0 +1,35 @@
+import json
+
+
+def get_vott_info():
+
+    info_map = dict()
+
+    with open('vott-json-export/PlasticBottlesMarking-export.json') as f:
+        data = json.load(f)
+
+    # Prolazak kroz sve slike
+    for asset_id, asset_data in data['assets'].items():
+        # print('Asset data = ', asset_data)
+        image_name = asset_data['asset']['name']
+
+        info_map[asset_id] = image_name
+
+        regions = asset_data['regions']
+
+        # print(f"Slika: {image_name}")
+
+        # Prolazak kroz sve regije
+        for region in regions:
+            region_id = region['id']
+            region_type = region['type']
+            tags = region['tags']
+            bounding_box = region['boundingBox']
+
+            # print(f"Regija ID: {region_id}")
+            # print(f"Tip regije: {region_type}")
+            # print(f"Oznake: {tags}")
+            # print(f"Bounding Box: {bounding_box}")
+            # print()
+
+    return info_map
