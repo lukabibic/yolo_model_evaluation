84c8fb82baaec5982eea327ec615d60263be08da
lbibic <luka.krnjevo@gmail.com>
2023-05-27 19:27:15 +0200

    sync main with master

--- comparison.py
+++ comparison.py
@@ -9,35 +9,6 @@ from vott_reader import get_vott_info
 
 # Function to calculate IoU (Intersection over Union)
 def calculate_iou(boxA, boxB):
-    # # Convert YOLOv5 normalized coordinates to pixel coordinates
-    # # img_width, img_height = 1024, 1024  # Replace with the actual image dimensions
-    # x1 = int(boxA['x'])
-    # y1 = int(boxA['y'])
-    # w1 = int(boxA['w'])
-    # h1 = int(boxA['h'])
-    #
-    # # Calculate the coordinates of the VOTT tagged region
-    # x2 = int(boxB['x'])
-    # y2 = int(boxB['y'])
-    # w2 = int(boxB['w'])
-    # h2 = int(boxB['h'])
-    #
-    # # Calculate the intersection coordinates
-    # x_intersection = max(x1, x2)
-    # y_intersection = max(y1, y2)
-    # w_intersection = min(x1 + w1, x2 + w2) - x_intersection
-    # h_intersection = min(y1 + h1, y2 + h2) - y_intersection
-    #
-    # # Calculate the areas of intersection and union
-    # intersection = w_intersection * h_intersection
-    # area1 = w1 * h1
-    # area2 = w2 * h2
-    # union = area1 + area2 - intersection
-    #
-    # # Calculate the IoU value
-    # iou = intersection / union
-    #
-    # return iou
 
     # Convert boxes to Shapely Polygon objects
     poly1 = box(boxA['x'], boxA['y'], boxA['x'] + boxA['w'], boxA['y'] + boxA['h'])
@@ -131,6 +102,12 @@ asset_txt_map = dict()
 
 info_map = get_vott_info()
 yolo_folder = 'yolo_detection_txt_files_2'
+
+iou_list = []
+
+yolo_detected_object_per_image_list = []
+vott_objects_per_image_list = []
+
 for asset_id in info_map:
     txt_file = match_vott_to_yolo(info_map[asset_id])
     yolo_path = os.path.join(yolo_folder, txt_file)
@@ -140,15 +117,20 @@ for asset_id in info_map:
         asset_txt_map[asset_id] = txt_file
     else:
         # print('file does not exist in folder!')
+        vott_file_path = 'vott_assets/' + asset_id + '-asset.json'
+
+        # Read VOTT tagged regions
+        vott_tagged_regions = read_vott_tagged(vott_file_path)
+        vott_tagged_regions_size = len(vott_tagged_regions)
+
+        vott_objects_per_image_list.append(vott_tagged_regions_size)
+
         pass
 
 try:
 
     for asset_id in asset_txt_map:
 
-
-        # yolo_file_path = 'yolo_detection_txt_files/DALL_C2_B7E_202023-05-08_2015.19.47_20-_20Plastic_20bottle.txt'
-        # vott_file_path = '220bcda86685baa05eae3d0d5eb5d608-asset.json'
         yolo_file_path = yolo_folder + '/' + asset_txt_map[asset_id]
         vott_file_path = 'vott_assets/' + asset_id + '-asset.json'
 
@@ -161,26 +143,25 @@ try:
             yolo_detections = read_yolo_detection(yolo_file_path)
             yolo_detections_size = len(yolo_detections)
 
+            yolo_detected_object_per_image_list.append(yolo_detections_size)
+
             # Read VOTT tagged regions
             vott_tagged_regions = read_vott_tagged(vott_file_path)
             vott_tagged_regions_size = len(vott_tagged_regions)
 
+            vott_objects_per_image_list.append(vott_tagged_regions_size)
 
-            # print('Detection = ', yolo_detections)
-            # print('Region = ', vott_tagged_regions)
+            print('Detection = ', yolo_detections)
+            print('Region = ', vott_tagged_regions)
 
             if yolo_detections_size == 1 and vott_tagged_regions_size == 1:
-
-                # ADJUST Y TO BE THE SAME AS VOTT FOR TESTING PURPOSE
-                # yolo_detections[0]['y'] = vott_tagged_regions[0]['y']
-                # yolo_detections[0]['x'] = vott_tagged_regions[0]['x']
 
                 # Compare detections and tagged regions
                 for detection in yolo_detections:
                     for region in vott_tagged_regions:
                         # Compare detection and region based on label and bounding box
-                        print('Detection = ', detection)
-                        print('Region = ', region)
+                        # print('Detection = ', detection)
+                        # print('Region = ', region)
 
                         if str.lower(detection['label']) == str.lower(region['label']):
                             print('CORRECT DETECTION')
@@ -195,38 +176,86 @@ try:
                             # Print IoU for demonstration
                             print(f"IoU: {iou}")
 
+                        iou_list.append(iou)
+
                     # Perform comparison and evaluation calculations here
                     # You can calculate metrics like IoU, precision, recall, etc.
             elif yolo_detections_size == vott_tagged_regions_size:
-                yolo_bottles_count, vott_bottles_count = 0, 0
-                print('SAME SIZE BUT COMPARISON LOGIC MISSING')
-                print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-                for detection in yolo_detections:
-                    for region in vott_tagged_regions:
 
-                        if str.lower(detection['label']) == 'bottle':
-                            yolo_bottles_count += 1
-                        vott_bottles_count += 1
+                # Sort detections and regions based on the combined ('x', 'y') value
+                sorted_detections = sorted(yolo_detections, key=lambda d: (d['x'], d['y']))
+                sorted_regions = sorted(vott_tagged_regions, key=lambda r: (r['x'], r['y']))
 
-                print('SAME SIZE FOUND THIS MANY BOTTLES:', yolo_bottles_count)
+                # Now you can iterate over the sorted lists and compare the corresponding detections and regions
+                for detection, region in zip(sorted_detections, sorted_regions):
+                    if str.lower(detection['label']) == str.lower(region['label']):
+                        print('CORRECT DETECTION')
+                        iou = calculate_iou(detection, region)
 
-                print('Yolo found this many bottles : ', yolo_bottles_count)
-                print('Actual bottle count from VoTT : ', vott_bottles_count)
+                        # Print IoU for demonstration
+                        print(f"IoU: {iou}")
+                    elif detection['label'] != region['label']:
+                        print('INCORRECT DETECTION, detected object = ', detection['label'])
+
+                        iou = calculate_iou(detection, region)
+                        # Print IoU for demonstration
+                        print(f"IoU: {iou}")
+
+                    iou_list.append(iou)
+
             else:
-                print('Yolo tagged this many objects in photos : ', yolo_detections_size)
-                print('Actual count of tagged objects using VoTT : ', vott_tagged_regions_size)
 
-                yolo_bottles_count = 0
-                other_objects_count = 0
+                # Initialize a list to store the closest matching region for each detection
+                matching_regions = []
+
+                # Iterate over each detection
                 for detection in yolo_detections:
-                    if str.lower(detection['label']) == 'bottle':
-                        print('Some bottle detected')
-                        yolo_bottles_count += 1
-                    else:
-                        print('Wrong object detected = ', detection['label'])
-                        other_objects_count += 1
+                    best_iou = 0  # Initialize the best IoU to 0
+                    best_region = None  # Initialize the best matching region as None
+
+                    # Calculate IoU between the detection and each region
+                    for region in vott_tagged_regions:
+                        iou = calculate_iou(detection, region)  # Replace with your IoU calculation function
+
+                        # Update the best IoU and best matching region if the current IoU is higher
+                        if iou > best_iou:
+                            best_iou = iou
+                            best_region = region
+
+                    # Add the best matching region to the list
+                    matching_regions.append(best_region)
+
+                # Now you can iterate over the detections and matching_regions lists and compare the corresponding objects
+                for i, detection in enumerate(yolo_detections):
+                    print('DETECTION = ', detection)
+                    print('MATCHING REGION = ', matching_regions[i])
+
+                    if str.lower(detection['label']) == str.lower(matching_regions[i]['label']):
+                        print('CORRECT DETECTION')
+                        iou = calculate_iou(detection, matching_regions[i])
+
+                        # Print IoU for demonstration
+                        print(f"IoU: {iou}")
+                    elif str.lower(detection['label']) == str.lower(matching_regions[i]['label']):
+                        print('INCORRECT DETECTION, detected object = ', detection['label'])
+
+                        iou = calculate_iou(detection, matching_regions[i])
+                        # Print IoU for demonstration
+                        print(f"IoU: {iou}")
+
+                    iou_list.append(iou)
+
         print()
         print()
         print()
 except Exception as e:
     print(e)
+
+if len(iou_list) > 0:
+    print('AVERAGE IOU IN MODEL = ', sum(iou_list)/len(iou_list))
+
+print('YOLO objects count for all images = ', sum(yolo_detected_object_per_image_list))
+print('ACTUAL objects count for all images = ', sum(vott_objects_per_image_list))
+
+print('VOTT FILES COUNT = ', len(vott_objects_per_image_list))
+print('YOLO DIDNT DETECT ANY OBJECTS ON THIS AMOUNT OF IMAGE FILES = ', len(vott_objects_per_image_list) - len(yolo_detected_object_per_image_list))
